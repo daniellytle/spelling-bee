4a781338941eb93831a93f65e757d10221241236
Daniel Wilson <danielwilson@Daniels-MacBook-Air.local>
2024-02-29 09:10:27 -0700

    adding cookie game history and using one game per day

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { SpellingBee } from "spelling-bee-word-list"
 
 import GameController from "./GameController.js"
+import { format } from "date-fns"
 
 function App() {
-
-  const { letters, validWords } = new SpellingBee()
+  const todayString = format(new Date(), "MMM dd, yyyy")
+  const { letters, validWords } = new SpellingBee().getGameForDate(new Date())
 
   return (
     <div className="App touch-manipulation">
-      <header>
-        <h1 className='text-center py-4 md:py-10 text-4xl font-bold'>Free Bee</h1>
+      <header className="flex flex-col text-center py-4 md:py-10">
+        <span className="text-4xl font-bold">Free Bee</span>
+        <span className="">{todayString}</span>
       </header>
       <GameController letters={letters} validWords={validWords} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/GameController.js
+++ src/GameController.js
@@ -1,21 +1,36 @@
-import { useState, useRef } from "react"
+import { useState, useRef, useEffect } from "react"
+import { useCookies } from "react-cookie"
+import { format } from "date-fns";
 
 import LetterPicker from "./LetterPicker.js"
 import ScoreSheet from "./ScoreSheet.js"
 import HelpModal from "./HelpModal.js"
 
 function GameController({ letters, validWords }) {
-  const [foundWords, setFoundWords] = useState([])
+  const [cookies, setCookie] = useCookies(['foundWords', 'playingDate']);
+  const todayString = format(new Date(), "yyyy-MM-dd")
+  if (cookies['playingDate'] !== todayString) {
+    setCookie('playingDate', todayString)
+    setCookie('foundWords', [])
+  }
+
+  const [foundWords, setFoundWords] = useState(cookies['foundWords'])
   const [score, setScore] = useState(0)
   const inputRef = useRef([null])
+
+  useEffect(() => {
+    setScore(foundWords.map((word) => getWordScore(word)).reduce((sum, x) => sum + x, 0))
+  }, [foundWords])
 
   const submitWord = (str) => {
     if (foundWords.includes(str)) {
       return [false, "Already found"]
     } else if (validWords.has(str)) {
       const wordScore = getWordScore(str)
+      const updatedFoundWords = foundWords.concat(str).sort()
+      setCookie('foundWords', updatedFoundWords)
       setScore(score + wordScore)
-      setFoundWords(foundWords.concat(str).sort())
+      setFoundWords(updatedFoundWords)
       return [true, wordScore]
     } else if (str.length < 4) {
       return [false, "Too short"]
@@ -31,7 +46,7 @@ function GameController({ letters, validWords }) {
   const getWordScore = (word) => {
     if (letters.every((char) => word.includes(char))) {
       return 14
-    } else if (word.length == 4) {
+    } else if (word.length === 4) {
       return 1
     } else {
       return word.length
